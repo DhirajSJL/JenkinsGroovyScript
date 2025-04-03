241e25f5ba50c1a511bca2cca644c8b1f9aa3b29
Dhirajkumar <dhiraj6.lulla@rediffmail.com>
2025-04-03 14:19:42 +0530

    SSH Agent Execution

--- JenkinsFile.groovy
+++ JenkinsFile.groovy
@@ -7,5 +7,14 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/DhirajSJL/JenkinsFrontEndApp.git'
             }
         }
+        stage ('Deploy')
+        {
+            steps{
+                sshagent(['RemoteServer']) {
+                    sh 'ssh -o StrictHostKeyChecking=no ubuntu@23.23.8.104 "sudo rm -r /var/www/html/index.*"'
+                    sh 'scp -o StrictHostKeyChecking=no -r * ubuntu@23.23.8.104:/home/ubuntu/index.html'       
+                    }
+            }
+        }
     }
 }
