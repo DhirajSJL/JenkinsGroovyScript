a4ffbb87611cece96cf2229ff06d5205796011d4
Dhirajkumar <dhiraj6.lulla@rediffmail.com>
2025-04-24 15:39:33 +0530

    Jenkins Pipeline for Docker Execution

---
+++ JenkinsDocker.groovy
@@ -0,0 +1,32 @@
+pipeline {
+    agent any
+        tools {
+            maven 'Maven_3.9.8_for_java'
+    }
+    stages {
+            stage ('Checkout')
+            {
+                steps {
+                    git branch: 'main', url: 'https://github.com/DhirajSJL/Docker_Stuff.git'
+                }
+            }
+            stage ('Build')
+            {
+                steps{
+                    sh 'mvn clean package'
+                }
+            }
+            stage ('Deploy')
+            {
+                steps 
+                {
+                    sshagent(['JenkinsDocker'])
+                    {
+                    sh 'scp -o StrictHostKeyChecking=no target/*.jar ubuntu@13.234.119.31:/home/ubuntu/'
+                    //sh 'ssh -o StrictHostKeyChecking=no ubuntu@13.201.131.117 "sudo mv /home/ubuntu/*.war /opt/tomcat/webapps/Java_app.war"'
+                    //sh 'ssh -o StrictHostKeyChecking=no ubuntu@13.201.131.117 "sudo systemctl restart tomcat"'
+                    }
+                }
+            }
+}
+}
