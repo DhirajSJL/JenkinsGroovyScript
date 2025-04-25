146ef67d8d8aef1d1ed6eeae70768d457e953d8c
Dhirajkumar <dhiraj6.lulla@rediffmail.com>
2025-04-25 14:31:02 +0530

    Docker server IP Chnaged

--- JenkinsDocker.groovy
+++ JenkinsDocker.groovy
@@ -22,13 +22,13 @@ pipeline {
             steps {
                 sshagent(['JenkinsDocker']) {
                     // Transfer files to remote server
-                    sh 'scp -o StrictHostKeyChecking=no target/*.jar ubuntu@13.234.119.31:/home/ubuntu/Java_code'
-                    sh 'scp -o StrictHostKeyChecking=no Dockerfile ubuntu@13.234.119.31:/home/ubuntu/Java_code'
-                    sh 'scp -o StrictHostKeyChecking=no docker-compose.yml ubuntu@13.234.119.31:/home/ubuntu/Java_code'
+                    sh 'scp -o StrictHostKeyChecking=no target/*.jar ubuntu@65.0.86.49:/home/ubuntu/Java_code'
+                    sh 'scp -o StrictHostKeyChecking=no Dockerfile ubuntu@65.0.86.49:/home/ubuntu/Java_code'
+                    sh 'scp -o StrictHostKeyChecking=no docker-compose.yml ubuntu@65.0.86.49:/home/ubuntu/Java_code'
 
                     // Execute docker-compose commands on the remote server
                     sh '''
-                        ssh -o StrictHostKeyChecking=no ubuntu@13.234.119.31 '
+                        ssh -o StrictHostKeyChecking=no ubuntu@65.0.86.49 '
                             cd /home/ubuntu/Java_code &&
                             docker-compose down || true &&
                             docker-compose up -d --build
