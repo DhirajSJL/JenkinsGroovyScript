6fd01a4406bf1b370157144bedd55016750c87a3
Dhirajkumar <dhiraj6.lulla@rediffmail.com>
2025-04-25 14:20:05 +0530

    Executing Docker Compose

--- JenkinsDocker.groovy
+++ JenkinsDocker.groovy
@@ -1,34 +1,41 @@
 pipeline {
     agent any
-        tools {
-            maven 'Maven_3.9.8_for_Java'
+
+    tools {
+        maven 'Maven_3.9.8_for_Java'
     }
+
     stages {
-            stage ('Checkout')
-            {
-                steps {
-                    git branch: 'main', url: 'https://github.com/DhirajSJL/Docker_Stuff.git'
-                }
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/DhirajSJL/Docker_Stuff.git'
             }
-            stage ('Build')
-            {
-                steps{
-                    sh 'mvn clean package'
-                }
+        }
+
+        stage('Build') {
+            steps {
+                sh 'mvn clean package'
             }
-            stage ('Deploy')
-            {
-                steps 
-                {
-                    sshagent(['JenkinsDocker'])
-                    {
+        }
+
+        stage('Deploy') {
+            steps {
+                sshagent(['JenkinsDocker']) {
+                    // Transfer files to remote server
                     sh 'scp -o StrictHostKeyChecking=no target/*.jar ubuntu@13.234.119.31:/home/ubuntu/Java_code'
                     sh 'scp -o StrictHostKeyChecking=no Dockerfile ubuntu@13.234.119.31:/home/ubuntu/Java_code'
                     sh 'scp -o StrictHostKeyChecking=no docker-compose.yml ubuntu@13.234.119.31:/home/ubuntu/Java_code'
-                    //sh 'ssh -o StrictHostKeyChecking=no ubuntu@13.201.131.117 "sudo mv /home/ubuntu/*.war /opt/tomcat/webapps/Java_app.war"'
-                    //sh 'ssh -o StrictHostKeyChecking=no ubuntu@13.201.131.117 "sudo systemctl restart tomcat"'
-                    }
+
+                    // Execute docker-compose commands on the remote server
+                    sh '''
+                        ssh -o StrictHostKeyChecking=no ubuntu@13.234.119.31 '
+                            cd /home/ubuntu/Java_code &&
+                            docker-compose down || true &&
+                            docker-compose up -d --build
+                        '
+                    '''
                 }
             }
-}
+        }
+    }
 }
